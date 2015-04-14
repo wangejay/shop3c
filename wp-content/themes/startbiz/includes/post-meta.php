cbc6d6e99898790eee68d87e2c4cc66a22c6231f
Jackie <wangejay@gmail.com>
2015-04-14 16:39:38 +0800

    test

--- wp-content/themes/startbiz/includes/post-meta.php
+++ wp-content/themes/startbiz/includes/post-meta.php
@@ -22,7 +22,7 @@ if( ( stf_get_option( 'post_meta' ) && empty( $get_meta["stf_hide_meta"][0] ) ) 
 	<span>文章目錄 <?php printf('%1$s', get_the_category_list( ', ' ) ); ?></span>
 <?php endif; ?>	
 <?php if( stf_get_option( 'post_comments' ) ): ?>
-	<span><?php comments_popup_link( '發表留言', '1 則留言', '% 則留言'); ?></span>
+	<span><?php comments_popup_link( '發表留', '1 則留言', '% 則留言'); ?></span>
 	<span><?php the_views(); ?></span>
 <?php endif; ?>
 </p>
