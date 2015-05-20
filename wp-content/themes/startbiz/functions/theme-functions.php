7200669bcce9b0847b1b6280b55e0d19cf6c8559
Jackie <wangejay@gmail.com>
2015-05-20 10:02:50 +0800

    remove personal option

--- wp-content/themes/startbiz/functions/theme-functions.php
+++ wp-content/themes/startbiz/functions/theme-functions.php
@@ -658,12 +658,30 @@ function stf_show_extra_profile_fields( $user ) { ?>
 	</table>
 <?php }
 
-## [JW] remove admin_color_scheme 
-function admin_color_scheme() {
-   global $_wp_admin_css_colors;
-   $_wp_admin_css_colors = 0;
+
+// removes the `profile.php` admin color scheme options
+remove_action( 'admin_color_scheme_picker', 'admin_color_scheme_picker' );
+
+if ( ! function_exists( 'cor_remove_personal_options' ) ) {
+  /**
+   * Removes the leftover 'Visual Editor', 'Keyboard Shortcuts' and 'Toolbar' options.
+   */
+  function cor_remove_personal_options( $subject ) {
+    $subject = preg_replace( '#<h3>Personal Options</h3>.+?/table>#s', '', $subject, 1 );
+    return $subject;
+  }
+
+  function cor_profile_subject_start() {
+    ob_start( 'cor_remove_personal_options' );
+  }
+
+  function cor_profile_subject_end() {
+    ob_end_flush();
+  }
 }
-add_action('admin_head', 'admin_color_scheme');
+add_action( 'admin_head-profile.php', 'cor_profile_subject_start' );
+add_action( 'admin_footer-profile.php', 'cor_profile_subject_end' );
+
 
 
 ## 儲存使用者的社交網站帳號
